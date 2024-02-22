00ae5e0074664c13409d825d913cdbf6d34e8adb
Mehmet Berkan <mehmetberkanvdn@outlook.com>
2024-02-22 12:35:13 +0300

    Add project files.

---
+++ Constructors/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructors
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer = new Customer{Id = 1,FirstName = "Engin",LastName = "Demiroğ",City = "Ankara"};
+
+            Customer customer2 = new Customer(2, "Derin", "Demiroğ", "Ankara");
+
+            Console.WriteLine(customer2.FirstName);
+            
+            Console.ReadLine();
+        }
+
+    }
+
+
+    class Customer
+    {
+        public Customer()
+        {
+            
+        }
+        //default Constructor
+        public Customer(int id,string firstName,string lastName,string city)
+        {
+            this.Id = id;
+            FirstName = firstName;
+            LastName = lastName;
+            City = city;
+        }
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string City { get; set; }
+
+    }
+}
---
+++ Interfaces/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interfaces
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            //IPersonManager customManager = new CustomManager();
+            
+
+            //IPersonManager employeeManager = new EmployeeManager();
+            
+            ProjectManager projectManager = new ProjectManager();
+            projectManager.Add(new InternManager());
+
+            Console.ReadLine();
+        }
+    }
+
+    interface IPersonManager
+    {
+        void Add();
+        void Update();
+    }
+
+    class CustomManager : IPersonManager
+    {
+        public void Add()
+        {
+            Console.WriteLine("Müşteri Eklendi");
+        }
+
+        public void Update()
+        {
+            Console.WriteLine("Müşteri Güncellendi");
+        }
+    }
+
+    class EmployeeManager : IPersonManager
+    {
+        public void Add()
+        {
+            Console.WriteLine("Personel Eklendi");
+        }
+
+        public void Update()
+        {
+            Console.WriteLine("Personel Güncellendi");
+        }
+    }
+
+    class InternManager : IPersonManager
+    {
+        public void Add()
+        {
+            Console.WriteLine("Stajyer Eklendi.");
+        }
+
+        public void Update()
+        {
+            Console.WriteLine("Stajyer Güncellendi");
+        }
+    }
+
+    class ProjectManager
+    {
+        public void Add(IPersonManager personManager)
+        {
+            personManager.Add();
+        }
+
+        
+    }
+}
